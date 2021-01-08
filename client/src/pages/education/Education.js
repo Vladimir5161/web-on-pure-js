019cf073a63c9a2c9383a00b0754df169b195f87
vladimir <napoleon520@gmail.com>
2021-01-08 13:21:53 +0200

    added show more button for experiance page, added blocks of skills for project images(onclick) changed skills stars on skills page, added animation to project and work info blocks, added menu adaprivity for screens where screen width > screen height, remover img from menu header, changed font-family and added gifs as background

---
+++ client/src/pages/education/Education.js
@@ -0,0 +1,90 @@
+import Basic from "../Basic";
+import "./education.css";
+
+export class Education extends Basic {
+    static renderEducation(educationArray) {
+        const divEducations = document.createElement("div");
+        divEducations.className = "educations";
+        const currentH1 = document.createElement("div");
+        currentH1.className = "currentH1";
+        currentH1.innerText = "My education";
+        const educationId = document.createElement("div");
+        educationId.id = "education";
+
+        educationArray.map((item) => {
+            const edu = document.createElement("div");
+            edu.className = "educationBlockStart";
+            const educationName = document.createElement("div"); // name
+            educationName.className = "educationName";
+            educationName.innerText = item.name;
+            const educationText = document.createElement("div"); // text
+            educationText.className = "educationText";
+            educationText.innerText = item.text;
+
+            const educationImageBlock = document.createElement("div");
+            educationImageBlock.className = "educationImageBlock";
+            const educationImg = document.createElement("img");
+            educationImg.className = "educationImg";
+            
+            Object.assign(educationImg, {
+                src: item.src,
+            });
+            const educationImgStart = document.createElement("img");
+            educationImgStart.className = "educationImgClosed";
+            educationImgStart.id = 'educationImgClosed';
+            Object.assign(educationImgStart, {
+                src: item.src,
+            });
+            educationImageBlock.appendChild(educationImg);
+            educationImageBlock.appendChild(educationImgStart);
+            edu.appendChild(educationName);
+            edu.appendChild(educationText);
+            if(item.src !== "") {
+                edu.appendChild(educationImageBlock);
+            }
+            educationId.appendChild(edu);
+        });
+        divEducations.appendChild(currentH1);
+        divEducations.appendChild(educationId);
+
+        return divEducations;
+    }
+    static async render() {
+        const educationArray = await this.getContentApi("education");
+        const result = this.renderEducation(educationArray);
+        return result.outerHTML;
+    }
+}
+// this will add event listeners once content will be loaded
+const observeItem = main.querySelector(".wrapper");
+let observer = new MutationObserver((mutationRecords) => {
+    const renderEducation = main.querySelectorAll(".educationImageBlock");
+    if (mutationRecords[0].addedNodes[0].className === "educations") {
+        const allBlocks = main.querySelectorAll(".educationBlockStart");
+        for (let i = 0; i < allBlocks.length; i++) {
+            setTimeout(
+                () => (allBlocks[i].className = "educationBlock"),
+                200 * i
+            );
+        }
+            // logic about showing or hiding full size image window on eductation page, and do not running animation when pahe is loaded
+            for (let item of renderEducation) {
+                const imageDiv = item.querySelector("#educationImgClosed");
+                const setClassname = () => {
+                    imageDiv.className = imageDiv.className === "educationImgClosed"
+                            ? "educationImgOpened"
+                            : "educationImgClosed";
+
+                }
+                    imageDiv.addEventListener("mousedown", () => {
+                        setClassname()
+                    });
+            }
+        }
+    
+});
+
+observer.observe(observeItem, {
+    childList: true, // наблюдать за непосредственными детьми
+    characterDataOldValue: false,
+});
---
+++ client/src/pages/skills/Skill.js
@@ -0,0 +1,90 @@
+import Basic from "../Basic";
+import "./skills.css";
+
+export class Skill extends Basic {
+    static renderSkills(skillsArr) {
+        const skills = document.createElement("div");
+        skills.className = "skills";
+        const currentH1 = document.createElement("div");
+        currentH1.className = "currentH1";
+        currentH1.innerText = "My current Skills";
+        const skillBlock = document.createElement("div");
+        skillBlock.id = "skillBlock";
+        skillsArr.reverse().map((item) => {
+            const starsBack = document.createElement("div");
+            starsBack.className = 'starsBack'
+            for (let i = 0; i < item.stars; i++) {
+                const starBack = document.createElement("img");
+                Object.assign(starBack, {
+                    alt: "",
+                    width: "20",
+                    heigth: "20",
+                    src:
+                        "https://upload.wikimedia.org/wikipedia/commons/1/1f/Star_Icon.png",
+                    className: "starsDiv",
+                });
+                
+                starBack.onload = starsBack.appendChild(starBack);
+            }
+            if (!item.stars) return "";
+            let skillName = document.createElement("div");
+
+            skillName.className = "skillNameDiv";
+
+            skillName.innerText = item.name;
+            let skillDiv = document.createElement("div");
+            skillDiv.className = "skillDivStart";
+
+            let skillDivContentBack = document.createElement("div");
+            let skillDivContent = document.createElement("div");
+            skillDivContent.className = "skillDivContent";
+            skillDivContentBack.className = "skillDivContentBack";
+            skillDivContent.appendChild(skillName);
+            skillDivContentBack.appendChild(starsBack);
+            skillDiv.addEventListener("click", () => {
+                item.className = "skillDivAnimation";
+            });
+            skillDiv.appendChild(skillDivContentBack);
+            skillDiv.appendChild(skillDivContent);
+            skillBlock.appendChild(skillDiv);
+        });
+        skills.appendChild(currentH1);
+        skills.appendChild(skillBlock);
+        return skills;
+    }
+    static async render() {
+        const skillsArr = await this.getContentApi("skills");
+        const result = this.renderSkills(skillsArr);
+        return result.outerHTML;
+    }
+}
+
+// this will add event listeners once content will be loaded
+const observeItem = main.querySelector(".wrapper");
+let observer = new MutationObserver((mutationRecords) => {
+    const renderSkill = main.querySelectorAll(".skillDivStart");
+    if (mutationRecords[0].addedNodes[0].className === "skills") {
+        const rend = renderSkill.length;
+        for (let i = rend - 1; i < rend && i >= 0; i--) {
+            setTimeout(
+                () => (renderSkill[i].className = "skillDiv"),
+                1000 - i * 50
+            );
+        }
+        for (let item of renderSkill) {
+            item.addEventListener("click", () => {
+                Object.assign(item, {
+                    className:
+                        item.className === "skillDivAnimation"
+                            ? "skillDivAnimationRotate"
+                            : "skillDivAnimation",
+                });
+            });
+        }
+    }
+});
+
+observer.observe(observeItem, {
+    childList: true, // наблюдать за непосредственными детьми
+    characterDataOldValue: false,
+});
